e4162c326c8eccf58ff24cfdbc4c6f2733be7281
Zacharie Makeen <zach.makeen@gmail.com>
2021-03-15 19:42:23 -0400

    Download HTML files

--- collect_covid_data.py
+++ collect_covid_data.py
@@ -1,30 +1,33 @@
 import urllib
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+import datetime
 
 
 class CollectData:
     def __init__(self):
-        self.url = "https://www.worldometers.info/coronavirus/"
-        self.soup = self.__parse_html()
-        print(self.soup)
-        # self.__store_html()
+        self.__url = "https://www.worldometers.info/coronavirus/"
+        self.__download_html()
+        # self.__soup = self.__parse_html()
+        # self.__store_data()
 
-    def __parse_html(self):
+    def __download_html(self):
         try:
-            request = Request(self.url, headers={'User-Agent': 'Mozilla/5.0'})
+            request = Request(self.__url, headers={'User-Agent': 'Mozilla/5.0'})
             page = urlopen(request)
             html = page.read().decode("utf-8")
-            return BeautifulSoup(html, "html.parser")
+            soup = BeautifulSoup(html, "html.parser")
+            with open("local_html/" + f"local_file{datetime.date.today()}.html", "w", encoding="utf-8") as file:
+                file.write(soup.prettify())
         except urllib.error.HTTPError as error:
             print(error)
 
-    # def __parse_html_test(self):
-    #     return BeautifulSoup(open("website/Coronavirus Update (Live)_ 120,669,548 Cases and 2,669,118 Deaths from COVID-19 Virus Pandemic - Worldometer.html", encoding="utf8"), "html.parser")
+    def __parse_html(self):
+        with open("html-pages/2021-03-15.html", encoding="utf-8") as html:
+            return BeautifulSoup(html.read(), "html.parser")
 
-    def store_data(self):
-        htmllist = self.soup.find_all("tr", style="")
-        print(htmllist)
+    def __store_data(self):
+        table = self.__soup.find(id="main_table_countries_today")
 
 # Path: <div class="tab_content" id="nav-tabContent">
 #           <div class="tab-pane active" id="nav-today" role="tabpanel" arialabelledby="nav-today-tab">
--- url_opener.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-
-class AppURLopener(urllib.request.FancyURLopener):
-    version = "Mozilla/5.0"
-
-
-opener = AppURLopener()
-response = opener.open("https://www.worldometers.info/coronavirus/")
