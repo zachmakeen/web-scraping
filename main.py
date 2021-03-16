02584f676f41949239a0a0a0409ed4f7251ce21f
Zacharie Makeen <zach.makeen@gmail.com>
2021-03-15 20:07:15 -0400

    Added interaction more

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from collect_covid_data import ScrapeData
 
-print('Type "help" to see more commands')
-command = ''
-while True:
-    command = input('> ').lower()
-    if command == 'help':
-        print('''
-        scrape - to scrape data
-        analyse - to analyse data
-        quit - to end the program
-        ''')
-    elif command == 'scrape':
-        ScrapeData()
-    elif command == 'analyse':
-        pass
-    elif command == 'quit':
-        break
-    else:
-        print('Sorry, that is not a valid command.')
+# print('Type "help" to see more commands')
+# command = ''
+# while True:
+#     command = input('> ').lower()
+#     if command == 'help':
+#         print('''
+#         scrape - to scrape data
+#         analyse - to analyse data
+#         quit - to end the program
+#         ''')
+#     elif command == 'scrape':
+#         ScrapeData()
+#     elif command == 'analyse':
+#         pass
+#     elif command == 'quit':
+#         break
+#     else:
+#         print('Sorry, that is not a valid command.')
 
 ScrapeData()
