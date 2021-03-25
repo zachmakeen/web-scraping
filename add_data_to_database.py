5e56070b3f11bc537c88fd066ead9ed60bbef3b9
rivasal.rodrigo <rodrigo.rivasalfaro@dawsoncollege.qc.ca>
2021-03-25 00:43:38 -0400

    modified schemas, added date to corona_table and new schema for country_borders

--- add_data_to_database.py
+++ add_data_to_database.py
@@ -4,19 +4,18 @@ import os
 
 class CreateDatabase:
 
-    def __init__(self, date):
-        self.__date = date
+    def __init__(self):
         self.__db_name = None  # 'covid_corona_db_MAKE_RIVA'
         self.__connection = self.__connect_to_db()
         # self.table_name = f'covid_data{self.__date}'
-        self.__table_schema = '(`rank`, `Country,Other`, `TotalCases`, `NewCases`, `TotalDeaths`, `NewDeaths`, `TotalRecovered`, `ActiveCases`, `Serious,Critical`, `Tot Cases/1M pop`, `Deaths/1M pop`, `TotalTests`, `Tests/1M pop`, `Population`, `Continent`, `1 Caseevery X ppl`, `1 Deathevery X ppl`, `1 Testevery X ppl`)'
+        # self.__table_schema = '(`rank`, `Country,Other`, `TotalCases`, `NewCases`, `TotalDeaths`, `NewDeaths`, `TotalRecovered`, `ActiveCases`, `Serious,Critical`, `Tot Cases/1M pop`, `Deaths/1M pop`, `TotalTests`, `Tests/1M pop`, `Population`, `Continent`, `1 Caseevery X ppl`, `1 Deathevery X ppl`, `1 Testevery X ppl`)'
         self.__data = None
         self.__table_name = None
 
-    def store_data(self, data):
+    def store_data(self, data, table):
         #     self.__create_db()
         #     self.__data = data
-        # self.__select_db()
+        self.__table_name = table
         self.__create_db_tables_keys()
         self.__populate_table(self.__table_name, data)
         self.__closeDatabase()
@@ -61,7 +60,7 @@ class CreateDatabase:
 
     def __populate_table(self, table_name, data):
         str_form = ",%s" * (len(data[0]) - 1)
-        statement = f'INSERT INTO `{table_name}` {self.__table_schema} VALUES (%s {str_form})'
+        statement = f'INSERT INTO `{table_name}` VALUES (%s {str_form})'
         try:
             mycursor = self.__connection.cursor()
             mycursor.executemany(statement, data)
@@ -75,9 +74,10 @@ class CreateDatabase:
         self.__db_name = 'covid_corona_db_MAKE_RIVA'
         self.__create_db()
         self.__select_db(self.__db_name)
-
-        self.__table_name = f'covid_data{self.__date}'
-        table_schema = """(
+        table_schema = ''
+        if self.__table_name == 'corona_table':
+            table_schema = """(
+            `Date` date NOT NULL,
             `rank` int(3) NOT NULL,
             `Country,Other` varchar(50) NOT NULL,
             `TotalCases` int(10) DEFAULT NULL,
@@ -85,6 +85,7 @@ class CreateDatabase:
             `TotalDeaths` int(10) DEFAULT NULL,
             `NewDeaths` int(10) DEFAULT NULL,
             `TotalRecovered` int(10) DEFAULT NULL,
+            `NewRecovered` int(10) DEFAULT NULL,
             `ActiveCases` int(10) DEFAULT NULL,
             `Serious,Critical` int(10) DEFAULT NULL,
             `Tot Cases/1M pop` int(10) DEFAULT NULL,
@@ -96,8 +97,15 @@ class CreateDatabase:
             `1 Caseevery X ppl` int(10) DEFAULT NULL,
             `1 Deathevery X ppl` int(10) DEFAULT NULL,
             `1 Testevery X ppl` int(10) DEFAULT NULL,
-            PRIMARY KEY (`Country,Other`))"""
+            PRIMARY KEY (`Country,Other`, `Date`))"""
+        else:
+            table_schema = """(
+            `Country,Other` varchar(50) NOT NULL,
+            `Neighbour` varchar(50) NOT NULL,
+            `Distance` int (10) DEFAULT NULL),
+            PRIMARY KEY (`Country,Other`, `Neighbour`))"""
         self.__create_table(self.__table_name, table_schema)
+
 
     def __closeDatabase(self):
         self.__connection.close()
